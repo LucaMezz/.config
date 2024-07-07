e5d6749f132c02ca9942e70eca564dd6fb5266b8
LucaMezz <lucamezza4@gmail.com>
2024-07-07 10:10:04 +1000

    added luasnips lua file for snippets

--- nvim/lua/neotex/plugins/luasnip.lua
+++ nvim/lua/neotex/plugins/luasnip.lua
@@ -2,6 +2,8 @@ return {
   "L3MON4D3/LuaSnip",
   event = { "BufReadPre", "BufNewFile" },
   config = function()
+    require("user.snips")
+
     require("luasnip.loaders.from_snipmate").load({ paths = "~/.config/nvim/snippets/" })
   end
 }
